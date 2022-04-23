ed37ec96affd72bc32a84bfc47ec8ee1eaf6aa36
junetwentytwo <38581941+junetwentytwo@users.noreply.github.com>
2022-04-23 19:02:42 +0800

    Add files via upload

---
+++ Surge/ProfileReload.js
@@ -0,0 +1,21 @@
+/*
+By：Pysta
+
+GitHub:https://github.com/mieqq/mieqq
+
+* [Panel]
+* 配置重载 = title=配置重载,content=配置重载,style=info,script-name=配置重载,update-interval=-1
+
+* [Script]
+* 配置重载=script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Profile-Reload.js,type=generic
+*/
+
+$httpAPI("POST", "/v1/profiles/reload", {}, data => {
+    $notification.post("Profile Reload","Profile Reload Success","")
+    $done({
+        title: "Profile Reload",
+        content: "Profile Reload Success",
+        icon: "terminal",
+        "icon-color": "#5AC8FA",
+     })
+    });
