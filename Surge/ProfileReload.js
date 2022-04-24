c80aa6e5df4d340dafdf211304fe5d2d1ded0f3b
junetwentytwo <38581941+junetwentytwo@users.noreply.github.com>
2022-04-24 15:21:18 +0800

    Update ProfileReload.js

--- Surge/ProfileReload.js
+++ Surge/ProfileReload.js
@@ -16,6 +16,6 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
         title: "Profile Reload",
         content: "Profile Reload Success",
         icon: "doc.text",
-        "icon-color": "#5AC8FA",
+        "icon-color": "#EAE5E3",
      })
     });
